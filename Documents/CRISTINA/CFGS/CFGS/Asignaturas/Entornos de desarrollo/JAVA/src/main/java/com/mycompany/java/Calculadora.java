74cfe42648b910acf4445236278c9f6f6dd812ea
Cristina <cristina.marce.acosta@gmail.com>
2021-03-08 19:43:56 +0000

    paso 3 y 4

--- Documents/CRISTINA/CFGS/CFGS/Asignaturas/Entornos de desarrollo/JAVA/src/main/java/com/mycompany/java/Calculadora.java
+++ Documents/CRISTINA/CFGS/CFGS/Asignaturas/Entornos de desarrollo/JAVA/src/main/java/com/mycompany/java/Calculadora.java
@@ -2,6 +2,48 @@
 package com.mycompany.java;
 
 public class Calculadora {
+
+    /**
+     * @return the operador1
+     */
+    public Integer getOperador1() {
+        return operador1;
+    }
+
+    /**
+     * @param operador1 the operador1 to set
+     */
+    public void setOperador1(Integer operador1) {
+        this.operador1 = operador1;
+    }
+
+    /**
+     * @return the operador2
+     */
+    public Integer getOperador2() {
+        return operador2;
+    }
+
+    /**
+     * @param operador2 the operador2 to set
+     */
+    public void setOperador2(Integer operador2) {
+        this.operador2 = operador2;
+    }
+
+    /**
+     * @return the operacion
+     */
+    public Integer getOperacion() {
+        return operacion;
+    }
+
+    /**
+     * @param operacion the operacion to set
+     */
+    public void setOperacion(Integer operacion) {
+        this.operacion = operacion;
+    }
     private Integer operador1;
     private Integer operador2;
     private Integer operacion;
